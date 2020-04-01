67ccd66db9c511232693ef2f8c8cd8fcf725fbd0
Artem Lyubimov <lyubimov@stanford.edu>
2020-03-31 17:52:59 -0700

    Created libtbx_refresh.py

---
+++ libtbx_refresh.py
@@ -0,0 +1,18 @@
+
+from __future__ import absolute_import, division, print_function
+
+# First need to find the location of setup.py.
+# We can't use '__file__' because of libtbx,
+# and 'import interceptor' doesn't exist yet.
+import libtbx.load_env
+
+intxr_dir = libtbx.env.find_in_repositories(relative_path="interceptor", test=os.path.exists)
+
+# Run setup.py
+import procrunner
+
+result = procrunner.run(
+    ["libtbx.python", "setup.py", "develop"], working_directory=intxr
+)
+if result.returncode:
+    exit("Error during Interceptor configuration")
