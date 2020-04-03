180537c7ae4237dcbe05179bb77dbce984f673d4
Artem Lyubimov <lyubimov@stanford.edu>
2020-04-02 17:10:39 -0700

    tinkering with setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     author="Artem Y. Lyubimov",
     author_email="lyubimov@stanford.edu",
-    version="0.1.0",
+    version="0.2.0",
     url="https://github.com/alyubimov/interceptor",
     license="BSD",
     install_requires=[],
