440e19e8cb866887bad326442fe5b309856a2895
Artem Lyubimov <lyubimov@stanford.edu>
2020-04-06 17:25:00 -0700

    commit slipped by, it seems

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     author="Artem Y. Lyubimov",
     author_email="lyubimov@stanford.edu",
-    version="0.4.10",
+    version="0.4.11",
     url="https://github.com/alyubimov/interceptor",
     license="BSD",
     install_requires=[],
