46b3431930b8aa0d22d874bcfbcbee9be056e623
Artem Lyubimov <lyubimov@stanford.edu>
2020-04-15 13:03:34 -0700

    made stream.receive() more generic

--- src/interceptor/connector/connector.py
+++ src/interceptor/connector/connector.py
@@ -195,7 +195,7 @@ class Reader(ConnectorBase):
         fstart = time.time()
         if self.args.stype.lower() == 'req':
           self.stream.send(b"Hello")
-        frames = self.stream.receive()
+        frames = self.stream.receive(copy=False)
         fel = time.time() - fstart
       except Exception as exp:
         print ('DEBUG: {} CONNECT FAILED! {}'.format(self.name, exp))
