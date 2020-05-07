c3a869b71470a52edc83d81ec3d3334a9deb1a19
Artem Lyubimov <lyubimov@stanford.edu>
2020-05-06 18:47:46 -0700

    more bugfixes

--- src/interceptor/__init__.py
+++ src/interceptor/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.13.8"
+__version__ = "0.14.3"
 
 import os
 import platform
--- src/interceptor/command_line/connector_run.py
+++ src/interceptor/command_line/connector_run.py
@@ -23,7 +23,7 @@ import argparse
 
 from interceptor import __version__ as intxr_version
 from interceptor import packagefinder
-from interceptor.connector.connector import Reader, Collector, Broker
+from interceptor.connector.connector import Reader, Collector, Connector
 
 
 class ExpandPresets(argparse.Action):
@@ -188,7 +188,9 @@ def entry_point():
         rank = comm_world.Get_rank()
         localhost = MPI.Get_processor_name().split('.')[0]
         if rank == 0:
-            script = Collector(comm=comm_world, args=args, localhost=localhost)
+            script = Connector(comm=comm_world, args=args, localhost=localhost)
+        elif rank == 1:
+            script = Collector(comm=comm_world, args=args)
         else:
             script = Reader(comm=comm_world, args=args)
         comm_world.barrier()
