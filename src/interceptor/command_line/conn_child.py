e1a1e1960f4035972a1035d844bd303825b53f36
Artem Lyubimov <lyubimov@stanford.edu>
2020-05-15 15:16:05 -0700

    first attempt at dynamic process spawning

---
+++ src/interceptor/command_line/conn_child.py
@@ -0,0 +1,199 @@
+from __future__ import absolute_import, division, print_function
+
+"""
+Author      : Lyubimov, A.Y.
+Created     : 03/31/2020
+Last Changed: 04/20/2020
+Description : ZMQ Connector launched with MPI. For example:
+
+To run on 10 cores at host 'bl121proc00', receiving from port 8121, running 
+only spotfinding, text output to stdout, not forwarding to a GUI:
+
+mpirun --map-by core --bind-to core -np 10 python connector --host 
+bl121proc00 --port 8121 --last_stage spotfinding --verbose
+
+The same run as above, with forwarding to a GUI on port 9998:
+
+mpirun --map-by core --bind-to core -np 10 python connector --host 
+bl121proc00 --port 8121 --last_stage spotfinding --verbose 
+--uihost=localhost --uiport=9998 --uistype='push'
+"""
+
+import argparse
+
+from interceptor import __version__ as intxr_version
+from interceptor import packagefinder
+from interceptor.connector.connector import Connector, Reader, Collector
+
+
+class ExpandPresets(argparse.Action):
+    def __init__(self, option_strings, dest, nargs=1, **kwargs):
+        self.filename = kwargs.pop('filename', None)
+        if not self.filename:
+            raise ValueError("Preset filename must be specified")
+        if nargs != 1:
+            raise ValueError("Nargs must be equal to one")
+        super(ExpandPresets, self).__init__(option_strings, dest, **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        config = packagefinder(self.filename, 'connector', read_config=True)
+        for key, value in config[values].items():
+            setattr(namespace, key, value)
+
+
+def parse_command_args():
+    """ Parses command line arguments (only options for now) """
+    parser = argparse.ArgumentParser(
+        prog="connector_run.py",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=("ZMQ Stream Connector"),
+        epilog=("\n{:-^70}\n".format("")),
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version="Interceptor v{}".format(intxr_version),
+        help="Prints version info of Interceptor",
+    )
+    parser.add_argument(
+        "-n", "--n_proc", type=int, default=10, help="Number of processors"
+    )
+    parser.add_argument(
+        "--host", type=str, default='localhost', help="ZMQ server to listen to",
+    )
+    parser.add_argument(
+        "--port", type=str, default=9999, help="Port to listen to"
+    )
+    parser.add_argument(
+        "--stype", type=str, default='req', help="Socket type"
+    )
+    parser.add_argument(
+        "--uihost", type=str, help="UI host server to send to"
+    )
+    parser.add_argument(
+        "--uiport", type=str, help="UI port to send to"
+    )
+    parser.add_argument(
+        "--uistype", type=str, help="UI socket type"
+    )
+    parser.add_argument(
+        "-t",
+        "--timeout",
+        type=int,
+        default=60,
+        help="ZMQ Request timeout in seconds (when data not coming)",
+    )
+    parser.add_argument(
+        "--last_stage",
+        type=str,
+        default="spotfinding",
+        help='"Spotfinding", "indexing", or "integration" works',
+    )
+    parser.add_argument(
+        "--phil",
+        type=str,
+        default=None,
+        help="Absolute path to file with PHIL settings",
+    )
+    parser.add_argument(
+        "--mpi_bind",
+        type=str,
+        nargs="*",
+        default=None,
+        help='List of cpus to which the processes will bind (e.g. "1-10,20-54");'
+             ' will supersede the --n_proc value even from preset',
+    )
+    parser.add_argument("--header", action="store_true", default=False)
+    parser.add_argument("--test", action="store_true", default=False)
+    parser.add_argument(
+        "--verbose", action="store_true", default=False, help="Print output to stdout"
+    )
+    parser.add_argument(
+        "--send", action="store_true", default=False, help="Forward results to GUI"
+    )
+    parser.add_argument(
+        "--debug", action="store_true", default=False, help="Run debug code"
+    )
+    parser.add_argument(
+        "-b",
+        "--beamline",
+        type=str,
+        filename='beamlines.cfg',
+        action=ExpandPresets,
+        default=argparse.SUPPRESS,
+        help='Beamline filename (e.g. "12-1") will select host and port',
+    )
+    parser.add_argument(
+        "-e",
+        "--experiment",
+        type=str,
+        filename='experiments.cfg',
+        action=ExpandPresets,
+        default=argparse.SUPPRESS,
+        help='Experiment preset (e.g. "injector") will select number of '
+             "processors and extent of processing",
+    )
+    parser.add_argument(
+        "-u",
+        "--ui",
+        type=str,
+        filename='ui.cfg',
+        action=ExpandPresets,
+        default=argparse.SUPPRESS,
+        help='UI preset (e.g. "gui") will select to which port and host the output '
+             "is sent",
+    )
+    parser.add_argument(
+        "-r",
+        "--record",
+        type=str,
+        default=None,
+        help="If filepath is supplied, record proc info into file",
+    )
+    parser.add_argument(
+        "--dry_run",
+        action="store_true",
+        default=False,
+        help="Print the full command-line and exit without running",
+    )
+    parser.add_argument(
+        "--time",
+        action="store_true",
+        default=False,
+        help="Measure time per frame and output when run is terminated",
+    )
+    parser.add_argument(
+        "--broker",
+        action="store_true",
+        default=False,
+        help="Insert a broker in between Readers and Splitter",
+    )
+    parser.add_argument(
+        "--drain",
+        action="store_true",
+        default=False,
+        help="In True, will not process received images; used for testing",
+    )
+
+    return parser
+
+
+def entry_point():
+    try:
+        from mpi4py import MPI
+    except ImportError as ie:
+        print("DEBUG: MPI NOT LOADED! {}".format(ie))
+        exit()
+    else:
+        args, _ = parse_command_args().parse_known_args()
+        localhost = "localhost"
+        comm = MPI.Comm.Get_parent()
+        script = Reader(comm=comm, args=args, localhost=localhost)
+        print ('DEBUG: process #{} spawned!'.format(comm.Get_rank()))
+        script.run()
+
+
+if __name__ == "__main__":
+    entry_point()
+
+# -- end
---
+++ src/interceptor/command_line/conn_launcher.py
@@ -0,0 +1,79 @@
+from __future__ import absolute_import, division, print_function
+
+"""
+Author      : Lyubimov, A.Y.
+Created     : 04/06/2020
+Last Changed: 05/15/2020
+Description : Launches multiple ZMQ Connector instances via MPI
+"""
+
+import os
+import time
+import procrunner
+
+from interceptor.command_line.connector_run import parse_command_args
+
+times = []
+
+
+def make_command_line(args):
+    # parse presets if appropriate
+    connector_commands = ["interceptor"]
+    for arg, value in vars(args).items():
+        if value and arg not in ['n_proc', 'mpi_bind']:
+            if value is True:
+                cmd_list = ["--{}".format(arg)]
+            else:
+                cmd_list = ["--{}".format(arg), value]
+            connector_commands.extend(cmd_list)
+    return connector_commands
+
+
+def entry_point():
+    args, _ = parse_command_args().parse_known_args()
+    iargs = make_command_line(args)
+    if not args.dry_run:
+        try:
+            from mpi4py import MPI
+        except ImportError as ie:
+            print("DEBUG: MPI NOT LOADED! {}".format(ie))
+            exit()
+        else:
+            localhost = MPI.Get_processor_name().split('.')[0]
+            NPROCS = args.n_procs
+            COMMAND = ['interceptor'] * NPROCS
+            ARGS = [iargs] * NPROCS
+            MAXPROCS = NPROCS
+            INFO = [MPI.INFO_NULL] * NPROCS
+
+            print ('DEBUG: SPAWNING {} PROCESSES'.format(NPROCS))
+
+            comm_world = MPI.COMM_WORLD
+            children = comm_world.Spawn_multiple(
+                COMMAND,
+                ARGS,
+                MAXPROCS,
+                info=INFO,
+                root=0,
+            )
+            children.Barrier()
+            children.bcast(localhost, root=0)
+
+
+
+def get_total_time(ln):
+    if "TIME" in ln:
+        if times:
+            delta = time.time() - times[0]
+            times.append(delta)
+        else:
+            times.append(time.time())
+
+
+# ---------------------------------------------------------------------------- #
+
+
+if __name__ == "__main__":
+    entry_point()
+
+# -- end
