7e3f97b67b9e5f7f3331b7d1ec2b477acccf07e5
Artem Lyubimov <lyubimov@stanford.edu>
2020-05-18 17:59:52 -0700

    version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     author="Artem Y. Lyubimov",
     author_email="lyubimov@stanford.edu",
-    version="0.18.0",
+    version="0.17.1",
     url="https://github.com/alyubimov/interceptor",
     license="BSD",
     install_requires=[],
@@ -23,8 +23,6 @@ setuptools.setup(
             "connector = interceptor.command_line.connector_run:entry_point",
             "connect_mpi = "
             "interceptor.command_line.connector_run_mpi:entry_point",
-            'interceptor = interceptor.command_line.conn_child:entry_point',
-            'interceptor_mpi = interceptor.command_line.conn_launcher:entry_point',
         ],
         "gui_scripts": [
             "intxr = interceptor.command_line.ui_run:entry_point",
@@ -37,8 +35,6 @@ setuptools.setup(
             "intxr = intxr",
             "connector = connector",
             "connect_mpi = connect_mpi",
-            "interceptor = interceptor",
-            "interceptor_mpi = interceptor_mpi",
         ],
     },
     scripts=[],
